ed30a6f765aa95c3e179b1d5b226f58d70f19c4a
subhransu <subhransusekharsahu29@gmail.com>
2022-06-22 22:04:18 +0530

    first

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require("express");
+// const connection=require("./sql")
+const router=require("./router/router")
+const {postdata,getdata,updatedata,deletedata}=require("./controller/mysql")
+const app =express()
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+app.use("/",router)
+
+
+
+
+
+
+module.exports=app;
